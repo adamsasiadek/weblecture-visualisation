7596b9f466fd7322c4626e37eb3448d4215ba59e
Adam Sasiadek <Adam Sasiadek>
2016-03-21 17:47:02 +0100

    Unique views per day.
    Cleaned the code up, changed the frequencies to be unique per student per day. Thus: Opening the lecture multiple times on one day, counts as one view.

--- weblecture-visualisation.r
+++ weblecture-visualisation.r
@@ -12,21 +12,21 @@
 rm(list=ls())
 
 # Libraries:
-require(readxl) # for reading XLS files
-require(stringr)
-library("lubridate")  #install.packages("lubridate") # For finding date within range
-library(ggplot2)
-require(scales) # For making plotting of different date ranges easier
+require(readxl) # For reading XLS files.
+require(stringr) # For easy string manipulation.
+library(lubridate) # For finding date within range.
+library(ggplot2) # For plotting.
+require(scales) # For making plotting of different date ranges easier.
 
 # First Plot: Line Plot showing total views per lecture per week.
 ## Loading and cleaning data
-data <- read_excel("data/WSR-weblectures-nodiff.xlsx", sheet = 7)
+data <- read_excel("data/WSR-weblectures-nodiff.xlsx", sheet = "Viewing Sessions")
 
 ### Reducing variables to only neccessary ones.
 
-data <- data[,c(1,2,4:7)]
+data <- data[,c("Presentation Title","Username","Opened", "Last Active", "Time Watched (h:mm:ss)", "Coverage (h:mm:ss)")]
 
-### Converting time/date veriables to chron objects for later calculations
+### Converting time/date veriables to date objects for later calculations
 ### Check origin date when importing. Can be different depending on software eg. EXCEL or CALC and OS!!
 origin_time <- dmy_hms("30-12-1899 00:00:00")
 data$Opened <- origin_time + ddays(data$Opened)
@@ -34,21 +34,26 @@ data$`Last Active` <- origin_time + ddays(data$`Last Active`)
 
 ### Creating variable with date of Video
 #### extracting date pattern
-date_of_lecture <- str_match(pattern = "....[:digit:]-[:digit:][:digit:][:digit:][:digit:]", string =data[,1])
-#### cleaning up white space
-date_of_lecture <- str_trim(str_replace(date_of_lecture, pattern = "^-",replacement = ""))
+date_of_lecture <- str_match(pattern = "[:digit:]{1,2}-[:digit:]{1,2}-[:digit:]{4}", string =data[,1])
 #### Converting to date format
 date_of_lecture <- dmy(date_of_lecture)
 data <- cbind(date_of_lecture,data)
 
 ### Preparing the data for plotting in ggplot2
 
-plotdf <- data.frame(date_of_lecture = date_of_lecture, date_opened = round_date(data[,4], "day"))
+plotdf <- data.frame(date_of_lecture = date_of_lecture, date_opened = round_date(data[,"Opened"], "day"),username=data["Username"])
+## Creating a table of frequencies and filtering out all zero occurences. 
 plotdf <- as.data.frame(table(plotdf))
+plotdf <- plotdf[plotdf["Freq"] != 0,]
+## By making another frequency table of the remaining lectures and views, we get unique views per lecture. 
+## If a student has watched a lecture twice on one day, this is counted as one view. 
+plotdf <- as.data.frame(table(plotdf[c("date_of_lecture","date_opened")]))
 plotdf$date_opened <- ymd(plotdf$date_opened)
-#date_for_plotting <- min(testdf[,2]) + c(-5:(max(testdf[,2]) - min(testdf[,2])+5)) * days(1)
 
 ### Plot of frequenties views of each lecture by date. 
-base <- ggplot(plotdf, aes(date_opened,Freq)) + geom_line() + scale_x_datetime(breaks=date_breaks("3 day"),minor_breaks = date_breaks("1 day")) + facet_grid(date_of_lecture ~ .) + theme(axis.text.x = element_text(angle = 90, hjust = 1), strip.text.y = element_text(size=6, angle= 0))
+base <- ggplot(plotdf, aes(date_opened,Freq)) + geom_line() + 
+  scale_x_datetime(breaks=date_breaks("3 day"),minor_breaks = date_breaks("1 day")) +
+  facet_grid(date_of_lecture ~ .) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1), strip.text.y = element_text(size=6, angle= 0))
 base
  
